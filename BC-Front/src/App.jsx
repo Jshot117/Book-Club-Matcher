ac9d9b8c099baae4b88b02b44ae818c3700ec55e
Daniel Jackson <iamdanielj@outlook.com>
2024-08-17 23:02:21 -0400

    Change this

--- BC-Front/src/App.jsx
+++ BC-Front/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Profile from "./Components/Profile";
 
 function App() {
-
   return (
     <>
+      <Profile></Profile>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
