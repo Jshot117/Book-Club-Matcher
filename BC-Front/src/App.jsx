b932173a60959e62c1b4f5e69361b1e8259e141c
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-08-17 23:09:40 -0400

    took away unused asset import

--- BC-Front/src/App.jsx
+++ BC-Front/src/App.jsx
@@ -1,6 +1,5 @@
 import bookList from './Components/book-list'
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Profile from "./Components/Profile";
