334bffa81cf7cfa97eebbe8e2fb15fb4dbfd6758
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-08-18 22:53:49 -0400

    updated loginForm component

--- BC-Front/src/Components/loginForm.jsx
+++ BC-Front/src/Components/loginForm.jsx
@@ -1,13 +1,21 @@
-//import { text } from "stream/consumers";
-
+import react, { useState } from 'react';
 
 export default function loginForm() {
+    const [inputText, setInputText] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("should now send to backend for authentication", inputText);
+        setInputText("");
+    };
+
     return (
         <div>
-            <form input="text" value = {"username"}>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={inputText} onChange={(e) => setInputText(e.target.value)} placeholder="Enter username"></input>
 
+                <button type="submit">submit</button>
             </form>
-            <button type="submit">submit</button>
         </div>
     );
 
